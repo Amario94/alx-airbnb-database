e4dc5600dd7314b796fe84c860de2888eb9b8243
Amario94 <anyaamarachi47@gmail.com>
2025-07-06 09:45:59 +0100

    Add JOIN queries

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,31 @@
+-- 1. INNER JOIN: Bookings and Users
+SELECT 
+  bookings.id AS booking_id,
+  bookings.property_id,
+  bookings.start_date,
+  bookings.end_date,
+  users.id AS user_id,
+  users.name AS user_name,
+  users.email
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
+
+-- 2. LEFT JOIN: Properties and Reviews
+SELECT 
+  properties.id AS property_id,
+  properties.name AS property_name,
+  reviews.id AS review_id,
+  reviews.rating,
+  reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+-- 3. FULL OUTER JOIN: All Users and Bookings
+SELECT 
+  users.id AS user_id,
+  users.name AS user_name,
+  bookings.id AS booking_id,
+  bookings.property_id,
+  bookings.start_date
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
